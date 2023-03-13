dea3391fd97ff160cfd52a69eec8a297dff70654
Samandar Panjiev <panjiyevs021@gmail.com>
2023-03-13 08:30:41 +0500

    hoo

--- config/config/settings.py
+++ config/config/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*', '34.207.212.17']
 
-
+# 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
 # Application definition
 
 INSTALLED_APPS = [
@@ -153,6 +153,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/static"),
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/' # 'http://myhost:port/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- config/config/urls.py
+++ config/config/urls.py
@@ -27,3 +27,4 @@ urlpatterns = [
     path('api/doc', schema_view),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

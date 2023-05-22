9c3da8fd9fa7703f6c2cba176b255af86f59e6fd
Samandar Panjiev <panjiyevs021@gmail.com>
2023-05-22 05:50:25 +0500

    changes

--- config/config/settings.py
+++ config/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$&8cxi#ykn=hq31z5d7049tywtdr=3mury+d_cw7ab6uwxsnah
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '52.70.35.8', '172.31.31.229', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '35.172.179.129', 'localhost']
 
 # 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
 # Application definition

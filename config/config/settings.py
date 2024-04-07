81095ab2c199cb8b783ceef729480f4be9d9813b
Samandar__e <59472493+samandar021@users.noreply.github.com>
2024-04-07 08:09:29 +0500

    .

--- config/config/settings.py
+++ config/config/settings.py
@@ -183,7 +183,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     # Tell Django where to look for React's static files (css, js)
